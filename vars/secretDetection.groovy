249c99089ef2b141e32d9e4b24c2ebcf8ea52728
Samuel McManus <samuelmcmanus819@gmail.com>
2024-10-22 16:20:49 -0500

    Update secretDetection.groovy

--- vars/secretDetection.groovy
+++ vars/secretDetection.groovy
@@ -4,7 +4,7 @@ def call() {
             curl -Lo trufflehog.tar.gz "https://github.com/trufflesecurity/trufflehog/releases/download/v3.82.12/trufflehog_3.82.12_linux_amd64.tar.gz"
             tar -xzf trufflehog.tar.gz && rm trufflehog.tar.gz
         """
-        sh "./trufflehog git file://. --fail"
-        // sh "./trufflehog git file://. --since-commit main --fail"
+        // sh "./trufflehog git file://. --fail"
+        sh "./trufflehog git file://. --since-commit main --fail"
     }
 }
